7d90538de4eb26e5c09653c2ee860f55690ddf55
xia-nn <3807914356@qq.com>
2026-08-15 11:48:26 +0800

    fix: add missing profiledef.sh and correct executable bits / line endings
    - Add iso/profiles/xos/profiledef.sh (Archiso profile definition)
    - Add syslinux + dosfstools to packages.x86_64 for BIOS/UEFI boot
    - Set executable bits (755) in git for shell scripts and hooks
    - Add .gitattributes to force LF line endings for Linux files

---
+++ iso/profiles/xos/profiledef.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+# X OS Archiso profile definition
+
+iso_name="xos"
+iso_label="XOS_$(date +%Y%m)"
+iso_publisher="X OS <https://github.com/xia-nn/x-os>"
+iso_application="X OS Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="xos"
+buildmodes=('iso')
+bootmodes=(
+  'bios.syslinux.mbr'
+  'bios.syslinux.eltorito'
+  'uefi-ia32.grub.eltorito'
+  'uefi-x64.grub.eltorito'
+  'uefi-ia32.systemd-boot.esp'
+  'uefi-x64.systemd-boot.esp'
+)
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+)
